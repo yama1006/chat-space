cf421ebd4b7a124b72112133e8549c0eb6e6bdcb
yama1006 <chanyamawork@gmail.com>
2019-08-17 22:12:49 +0900

    log-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'messages' => 'messages#index'
+  devise_for :users
+  root 'messages#index'
+  resources :users, only: [:edit, :update]
 end
