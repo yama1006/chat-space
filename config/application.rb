695b728bc3ddf826bf0b40c5364479c8b0f9190c
yama1006 <chanyamawork@gmail.com>
2019-08-20 21:23:53 +0900

    group-function

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -14,6 +13,6 @@ module ChatSpace
       g.helper false
       g.test_framework false
     end
-    config.i18n.default_locate = :ja
+    config.i18n.default_locale = :ja
   end
 end
